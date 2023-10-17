a088197364473e70afa5173068fefd9dc0f21796
CtFatalv <contact.fatalv@gmail.com>
2023-10-17 12:40:41 +0200

    Legacy City

--- resources/[core]/es_extended/config.lua
+++ resources/[core]/es_extended/config.lua
@@ -16,12 +16,12 @@ Config.Accounts             = {
 	}
 }
 
-Config.StartingAccountMoney = { bank = 50000 }
+Config.StartingAccountMoney = { bank = 1000 }
 
 Config.StartingInventoryItems = false -- table/false
 
 Config.DefaultSpawns = { -- If you want to have more spawn positions and select them randomly uncomment commented code or add more locations
-	{ x = 222.2027, y = -864.0162, z = 30.2922, heading = 1.0 },
+	{ x = -269.4, y = -955.3, z = 31.2, heading = 205.8 },
 	--{x = 224.9865, y = -865.0871, z = 30.2922, heading = 1.0},
 	--{x = 227.8436, y = -866.0400, z = 30.2922, heading = 1.0},
 	--{x = 230.6051, y = -867.1450, z = 30.2922, heading = 1.0},
@@ -36,11 +36,11 @@ Config.AdminGroups = {
 
 Config.EnablePaycheck            = true      -- enable paycheck
 Config.LogPaycheck               = false     -- Logs paychecks to a nominated Discord channel via webhook (default is false)
-Config.EnableSocietyPayouts      = false     -- pay from the society account that the player is employed at? Requirement: esx_society
+Config.EnableSocietyPayouts      = true     -- pay from the society account that the player is employed at? Requirement: esx_society
 Config.MaxWeight                 = 24        -- the max inventory weight without backpack
 Config.PaycheckInterval          = 7 * 60000 -- how often to recieve pay checks in milliseconds
 Config.EnableDebug               = false     -- Use Debug options?
-Config.EnableDefaultInventory    = true      -- Display the default Inventory ( F2 )
+Config.EnableDefaultInventory    = false      -- Display the default Inventory ( F2 )
 Config.EnableWantedLevel         = false     -- Use Normal GTA wanted Level?
 Config.EnablePVP                 = true      -- Allow Player to player combat
 
@@ -50,42 +50,43 @@ Config.DistanceGive              = 4.0   -- Max distance when giving items, weap
 
 Config.AdminLogging              = false -- Logs the usage of certain commands by those with group.admin ace permissions (default is false)
 
-Config.DisableHealthRegeneration = false -- Player will no longer regenerate health
-Config.DisableVehicleRewards     = false -- Disables Player Recieving weapons from vehicles
-Config.DisableNPCDrops           = false -- stops NPCs from dropping weapons on death
-Config.DisableDispatchServices   = false -- Disable Dispatch services
-Config.DisableScenarios          = false -- Disable Scenarios
-Config.DisableWeaponWheel        = false -- Disables default weapon wheel
-Config.DisableAimAssist          = false -- disables AIM assist (mainly on controllers)
-Config.DisableVehicleSeatShuff   = false -- Disables vehicle seat shuff
-Config.DisableDisplayAmmo  		 = false -- Disable ammunition display
+Config.DisableHealthRegeneration = true -- Player will no longer regenerate health
+Config.DisableVehicleRewards     = true -- Disables Player Recieving weapons from vehicles
+Config.DisableNPCDrops           = true -- stops NPCs from dropping weapons on death
+Config.DisableDispatchServices   = true -- Disable Dispatch services
+Config.DisableScenarios          = true -- Disable Scenarios
+Config.DisableWeaponWheel        = true -- Disables default weapon wheel
+Config.DisableAimAssist          = true -- disables AIM assist (mainly on controllers)
+Config.DisableVehicleSeatShuff   = true -- Disables vehicle seat shuff
+Config.DisableDisplayAmmo  		 = true -- Disable ammunition display
 Config.RemoveHudComponents       = {
-	[1] = false,                         --WANTED_STARS,
-	[2] = false,                         --WEAPON_ICON
-	[3] = false,                         --CASH
-	[4] = false,                         --MP_CASH
-	[5] = false,                         --MP_MESSAGE
-	[6] = false,                         --VEHICLE_NAME
-	[7] = false,                         -- AREA_NAME
-	[8] = false,                         -- VEHICLE_CLASS
-	[9] = false,                         --STREET_NAME
-	[10] = false,                        --HELP_TEXT
-	[11] = false,                        --FLOATING_HELP_TEXT_1
-	[12] = false,                        --FLOATING_HELP_TEXT_2
-	[13] = false,                        --CASH_CHANGE
-	[14] = false,                        --RETICLE
-	[15] = false,                        --SUBTITLE_TEXT
-	[16] = false,                        --RADIO_STATIONS
-	[17] = false,                        --SAVING_GAME,
-	[18] = false,                        --GAME_STREAM
-	[19] = false,                        --WEAPON_WHEEL
-	[20] = false,                        --WEAPON_WHEEL_STATS
-	[21] = false,                        --HUD_COMPONENTS
-	[22] = false,                        --HUD_WEAPONS
+	[1] = false, --WANTED_STARS,
+	[2] = true, --WEAPON_ICON
+	[3] = false, --CASH
+	[4] = false,  --MP_CASH
+	[5] = false, --MP_MESSAGE
+	[6] = true, --VEHICLE_NAME
+	[7] = true,-- AREA_NAME
+	[8] = true,-- VEHICLE_CLASS
+	[9] = true, --STREET_NAME
+	[10] = false, --HELP_TEXT
+	[11] = false, --FLOATING_HELP_TEXT_1
+	[12] = false, --FLOATING_HELP_TEXT_2
+	[13] = false, --CASH_CHANGE
+	[14] = false, --RETICLE
+	[15] = false, --SUBTITLE_TEXT
+	[16] = false, --RADIO_STATIONS
+	[17] = false, --SAVING_GAME,
+	[18] = true, --GAME_STREAM
+	[19] = false, --WEAPON_WHEEL
+	[20] = true, --WEAPON_WHEEL_STATS
+	[21] = true, --HUD_COMPONENTS
+	[22] = true, --HUD_WEAPONS
 }
 
-Config.SpawnVehMaxUpgrades       = true       -- admin vehicles spawn with max vehcle settings
-Config.CustomAIPlates            = '........' -- Custom plates for AI vehicles
+
+Config.SpawnVehMaxUpgrades = false -- admin vehicles spawn with max vehcle settings
+--Config.CustomAIPlates = 'ESX.A111' -- Custom plates for AI vehicles 
 -- Pattern string format
 --1 will lead to a random number from 0-9.
 --A will lead to a random letter from A-Z.
